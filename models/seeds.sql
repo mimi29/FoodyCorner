d89ac776d0763b00476bb4db114637060745525d
unknown <ming29398@gmail.com>
2018-11-30 21:55:09 -0800

    added data and api routes

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,22 +1,33 @@
 use foody_corner_db;
 
-INSERT INTO members (name, password)
-    VALUES("Mary", "password");
-INSERT INTO members (name, password)
-    VALUES("John", "password");
-INSERT INTO members (name, password)
-    VALUES("Bob", "password");
+INSERT INTO members (email, password)
+    VALUES("mary@gmail.com", "password");
+INSERT INTO members (email, password)
+    VALUES("john@gmail.com", "password");
+INSERT INTO members (email, password)
+    VALUES("bog@gmail.com", "password");
 
-INSERT INTO categories(name, description)
-    VALUES("Poutry", "Meat");
 INSERT INTO categories(name, description)
     VALUES("Appertize", "appertize");
 INSERT INTO categories(name, description)
+    VALUES("Desert", "pie, cake, cookies");
+INSERT INTO categories(name, description)
+    VALUES("Poutry", "Meat");
+INSERT INTO categories(name, description)
     VALUES("Seafood", "seafood");
 INSERT INTO categories(name, description)
-    VALUES("Desert", "pie, cake, cookies");
+    VALUES("Soup", "soup");
+INSERT INTO categories(name, description)
+    VALUES("Sandwich", "sandwich");
 
 INSERT INTO menus(name, description)
     VALUES("Reed", "Reed's'Menu");
 INSERT INTO menus(name, description)
     VALUES("Paul", "Paul's Menu");
+
+INSERT INTO recipes(name, description, ingredients, instructions, MemberId)
+	VAlUES("Ginger Cookies","Holiday cookies","#1 2 cups flour #2 1 tablespoon ginger #3 1 teaspoon soda #4. 1 teaspoon salt #5. 1 cup shortening #6. 1 cup sugar#6. 1 egg #7. 1.4 cup black molasses","Chill dough. Roll in small balls.  Shake in bag of sugar to coat. Bake in 350 degree oven for 12 to 15 minutes.",1);
+INSERT INTO recipes(name, description, ingredients, instructions, MemberId)
+	VALUES("Lemon Chicken","Chicken","#1 1 1/2 pounds chicken breast or chicken tenders, cut into chunks  #2 1/4 cup all-purpose unbleached flour  #3 Coarse salt #4 2 tablespoons wok or vegetable oil, 2 turns of the pan (preferred brand: House of Tsang) #5 1 tablespoon (a splash) white or rice wine vinegar #6 1/2 cup chicken broth or stock #7 8 ounces (1 cup) prepared lemon curd #8 1/4 cup hot water #9 1 lemon, zested #10 2 scallions, thinly sliced or 20 blades fresh chives, finely chopped","#1 Coat the chunked chicken lightly in flour, seasoned with a little salt. Heat a large skillet or a wok-shaped nonstick pan over high heat. Stir fry chicken until golden, 3 or 4 minutes. Remove chicken from the pan and return pan to heat. Reduce heat to medium #2 Add a splash of vinegar to the pan and let it evaporate. Add stock or broth to the pan and scrape up any drippings with a whisk. Thin curd by stirring in a little hot water. Add curd to broth and whisk to combine. Add chicken back to the pan and simmer for 1 to 2 minutes to thicken sauce and finish cooking chicken pieces through. Remove the pan from heat, add the scallions or chives and zest, and toss chicken pieces well to combine zest and scallions or chives evenly throughout the sauce.",1);
+INSERT INTO recipes(name, description, ingredients, instructions, MemberId)
+    VALUES("Philly Steak Sandwiches", "Philly Steak Sandwiches", "#1 1 tablespoon extra-virgin olive oil, 1 turn of the pan, #2 2 large yellow skinned onions, sliced very thin, #3 Coarse salt and pepper or steak seasoning blend, #4 2 teaspoons extra-virgin olive oil, 2 drizzles, #5 1 1/2 pounds lean beef tenderloin or sirloin, ask butcher to slice the meat very thin, #6 1 teaspoon garlic salt, #8 Freshly ground black pepper, #9 8 slices provolone cheese, from the deli counter, #10 4 soft Italian sandwich hogie rolls, split 6 to 8 inches long each, split lengthwise", "#1 Heat a medium saucepan over medium high heat. Add 1 tablespoon extra-virgin olive oil, 1 turn of the pan. Add onions and season with salt and pepper or steak seasoning blend. Cook onions, stirring occasionally, 10 minutes or until onions are soft and caramel in color. #2 Heat a heavy griddle pan over medium high to high heat. Wipe griddle with a drizzle of oil using a paper towel. Sear and cook thin sliced steaks until brown but not crisp, about 2 minutes on each side. Cook steaks in single layers in 2 batches and tenderize by cutting into meat with the side of your spatula while they cook. When the steaks are browned, just before you remove them from heat, sprinkle them with garlic salt and pepper. When all of the steaks are cooked, line each of your split rolls with 2 slices of provolone cheese. Pile 1/4 of your meat and onions on to the griddle and mix together with your spatula. Pile the meat and onions into rolls on top of the cheese. The heat from the meat and onions will melt the cheese. Repeat for remaining servings and serve.", 1);
