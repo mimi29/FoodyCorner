c58c4578159ddaca4147da5accf0c60dfc55952a
unknown <ming29398@gmail.com>
2018-12-05 17:27:47 -0800

    added basic schema and table

---
+++ models/seeds-init.sql
@@ -0,0 +1,14 @@
+use foody_corner_db;
+
+INSERT INTO categories(name)
+    VALUES("Appertizer");
+INSERT INTO categories(name)
+    VALUES("Poutry");
+INSERT INTO categories(name)
+    VALUES("Seafood");
+INSERT INTO categories(name)
+    VALUES("Soup");
+INSERT INTO categories(name)
+    VALUES("Sandwich");
+INSERT INTO categories(name)
+    VALUES("Dessert");
